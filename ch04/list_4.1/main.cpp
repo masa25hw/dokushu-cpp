9e391d9b0a99992bc7c19052c80a6c66c6eded43
masanao-kobayashi <masanao.kobayashi@woven-planet.global>
2023-03-31 02:07:05 +0900

    feat: Self-learning

--- ch04/list_4.1/main.cpp
+++ ch04/list_4.1/main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 
+
+/*
+これらの識別子を直接使うことは少なく、多くの場合プリプロセッサ命令と組み合わせて使ったり、
+デバッグのためにコンソール出力を使ったりして、今何を実行しているのかを確かめるために利用する。
+ */
+class S
+{
+    public:
+        void foo();
+        void stdc_version();
+};
+
+void S::foo()
+{
+    std::cout << "__func__：" << __func__ << std::endl;
+}
+
+
+void S::stdc_version()
+{
+    #ifdef __STDC__
+        puts("Conforms to standards.");
+    #else
+        puts("Not standard compliant.");
+    #endif
+    #ifdef __STDC_VERSION__
+        printf("%ld\n", __STDC_VERSION__);
+    #else
+        printf("__STDC_VERSION__ undefined.\n");
+    #endif
+}
+
+
 int main()
 {
     // このソースファイル名を出力
@@ -19,4 +52,8 @@ int main()
     // lineは現在の行番号ではなく、変数に格納されたときの行番号が表示されることに注意
     std::cout << "line：" << line << ", __LINE__：" << __LINE__
         << std::endl;
+
+    S s;
+    s.foo(); // test
+    s.stdc_version(); // test
 }
